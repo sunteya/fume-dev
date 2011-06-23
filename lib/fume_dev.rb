bc6d169395397eb8582cab3a78c07ff77b14d40c
Sunteya <Sunteya@gmail.com>
2011-06-23 14:54:03 +0800

    require rspec-rails

--- lib/fume_dev.rb
+++ lib/fume_dev.rb
@@ -4,6 +4,8 @@ require "itslog"
 require "fabrication"
 require "ffaker"
 
+require "rspec-rails"
+
 module Fume
   module Dev
     VERSION = File.read(File.expand_path("../../VERSION", __FILE__)).chomp
